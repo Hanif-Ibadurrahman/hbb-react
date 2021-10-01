241b271d99113e0104c8b7d3fcd784b4bafee38a
salsabillah amru <slsbllhmr@gmail.com>
2021-10-01 01:40:18 -0700

    create area page

---
+++ src/app/pages/AreaPage/components/Modalform.tsx
@@ -0,0 +1,26 @@
+import { Form, Modal, Container, Row, Col, Button } from 'react-bootstrap';
+export function ModalForm(props) {
+  return (
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Tambah</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="show-grid">
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Nama Record Center</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="info" onClick={props.onHide}>
+          Tambah
+        </Button>
+        <Button variant="danger" onClick={props.onHide}>
+          Kembali
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
