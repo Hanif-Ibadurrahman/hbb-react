fc1c65e9b7da6fe42f3261140b4ad76509c9b8b9
Eda <mdechalidate@gmail.com>
2021-10-12 18:19:00 +0700

    unfinished listing and page tracking

--- src/app/components/Sidebar/SidebarData.tsx
+++ src/app/components/Sidebar/SidebarData.tsx
@@ -21,75 +21,38 @@ export const DashboardData = [
   },
 ];
 
-export const PermintaanData = [
+export const PeminjamanData = [
   {
-    title: 'Riwayat Peminjaman',
+    title: 'Request Box',
     icon: '',
-    link: '/peminjaman',
+    link: '/BoxPage',
   },
   {
-    title: 'Jatuh Tempo',
+    title: 'Pickup Box',
     icon: '',
-    link: '/tempo',
+    link: '/PickupBox',
   },
   {
-    title: 'Customer Borrow Box',
+    title: 'Pinjam Item',
     icon: '',
-    link: '/Customer/Borrow-Box',
+    link: '/PinjamItem',
+  },
+  {
+    title: 'Pengembalian Item',
+    icon: '',
+    link: '/PengembalianItem',
   },
 ];
 
+// export const ApprovalData = [
+//   {
+//     title: 'Approval Box',
+//     icon: '',
+//     link: '/BoxPage',
+//   },
+// ];
+
 export const MasterData = [
-  {
-    title: 'Berkas',
-    icon: '',
-    link: '/berkas',
-  },
-  {
-    title: 'Dokumen',
-    icon: '',
-    link: '/document',
-  },
-  {
-    title: 'Dokumen Duplikast',
-    icon: '',
-    link: '/dokumen',
-  },
-  {
-    title: 'Daftar Musnah',
-    icon: '',
-    link: '/musnah',
-  },
-  {
-    title: 'Daftar Upload',
-    icon: '',
-    link: '/upload',
-  },
-  {
-    title: 'User',
-    icon: '',
-    link: '/user',
-  },
-  {
-    title: 'Divisi',
-    icon: '',
-    link: '/divisi',
-  },
-  {
-    title: 'Record Center',
-    icon: '',
-    link: '/record',
-  },
-  {
-    title: 'Room',
-    icon: '',
-    link: '/room',
-  },
-  {
-    title: 'Lemari',
-    icon: '',
-    link: '/LemariPage',
-  },
   {
     title: 'Box',
     icon: '',
@@ -101,8 +64,87 @@ export const MasterData = [
     link: '/folder',
   },
   {
+    title: 'Dokumen',
+    icon: '',
+    link: '/document',
+  },
+  {
+    title: 'Kabinet',
+    icon: '',
+    link: '/LemariPage',
+  },
+  {
+    title: 'Ruangan',
+    icon: '',
+    link: '/room',
+  },
+  {
+    title: 'Record Center',
+    icon: '',
+    link: '/record',
+  },
+  {
+    title: 'Mobil',
+    icon: '',
+    link: '/VehiclePage',
+  },
+  {
+    title: 'User',
+    icon: '',
+    link: '/user',
+  },
+  {
+    title: 'Divisi',
+    icon: '',
+    link: '/divisi',
+  },
+  // {
+  //   title: 'Berkas',
+  //   icon: '',
+  //   link: '/berkas',
+  // },
+  // {
+  //   title: 'Dokumen Duplikast',
+  //   icon: '',
+  //   link: '/dokumen',
+  // },
+  {
+    title: 'Daftar Musnah',
+    icon: '',
+    link: '/musnah',
+  },
+  // {
+  //   title: 'Daftar Upload',
+  //   icon: '',
+  //   link: '/upload',
+  // },
+  // {
+  //   title: 'User',
+  //   icon: '',
+  //   link: '/user',
+  // },
+
+  {
     title: 'Masa Restensi',
     icon: '',
     link: '/restensi',
+  },
+];
+
+export const CustomerMasterData = [
+  {
+    title: 'Box',
+    icon: '',
+    link: '/BoxPage',
+  },
+  {
+    title: 'Folder',
+    icon: '',
+    link: '/folder',
+  },
+  {
+    title: 'Dokumen',
+    icon: '',
+    link: '/document',
   },
 ];
--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,10 +1,14 @@
-// import * as React from 'react';
+import React, { useState } from 'react';
 import Logo from 'assets/images/logo.png';
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import './sidebar.scoped.scss';
 import 'react-pro-sidebar/dist/css/styles.css';
-import { DashboardData, PermintaanData, MasterData } from './SidebarData';
-// import { Link, NavLink } from 'react-router-dom';
-import React, { useState } from 'react';
+import {
+  DashboardData,
+  PeminjamanData,
+  MasterData,
+  CustomerMasterData,
+} from './SidebarData';
 import IconHome from 'assets/images/icon/icon-1.png';
 import IconUI from 'assets/images/icon/icon-2.png';
 import IconAdvanced from 'assets/images/icon/icon-3.png';
@@ -13,10 +17,170 @@ import IconTable from 'assets/images/icon/icon-5.png';
 
 export function Sidebar() {
   const [isActive] = useState<boolean>(true);
+  const [role, setRole] = useState('admin');
 
-  // const handleToggle = () => {
-  //   setActive(!isActive);
-  // };
+  const CustomerMenu = props => {
+    return (
+      <>
+        <MenuItem
+          id={window.location.pathname === '/' ? 'active' : ''}
+          className=" pos-r"
+          icon={<img src={IconHome} className="h-5" alt="awSnap" />}
+          onClick={() => {
+            window.location.pathname = '/';
+          }}
+        >
+          {''} Beranda
+        </MenuItem>
+        <SubMenu
+          className="pos-r"
+          icon={<img src={IconAdvanced} className="h-5" alt="awSnap" />}
+          title="Master"
+        >
+          {CustomerMasterData.map((val, key) => {
+            return (
+              <MenuItem
+                id={
+                  window.location.pathname.split(val.link).pop() ? '' : 'active'
+                }
+                className="pos-r"
+                icon={<img src={val.icon} className="h-5" alt="awSnap" />}
+                key={key}
+                onClick={() => {
+                  window.location.pathname = val.link;
+                }}
+              >
+                {''}
+                {val.title}
+              </MenuItem>
+            );
+          })}
+        </SubMenu>
+        <MenuItem
+          id={
+            window.location.pathname === '/Customer/Borrow-Box' ? 'active' : ''
+          }
+          className=" pos-r"
+          icon={<img src={IconForm} className="h-5" alt="awSnap" />}
+          onClick={() => {
+            window.location.pathname = '/Customer/Borrow-Box';
+          }}
+        >
+          {''} Peminjaman
+        </MenuItem>
+        <MenuItem
+          id={
+            window.location.pathname === 'Customer/Returning-Box'
+              ? 'active'
+              : ''
+          }
+          className=" pos-r"
+          icon={<img src={IconUI} className="h-5" alt="awSnap" />}
+          onClick={() => {
+            window.location.pathname = 'Customer/Returning-Box';
+          }}
+        >
+          {''} Pengembalian
+        </MenuItem>
+        <MenuItem
+          id={
+            window.location.pathname === 'Customer/Destroy-Suggestion'
+              ? 'active'
+              : ''
+          }
+          className=" pos-r"
+          icon={<img src={IconTable} className="h-5" alt="awSnap" />}
+          onClick={() => {
+            window.location.pathname = 'Customer/Destroy-Suggestion';
+          }}
+        >
+          {''} Usulan Penghapusan
+        </MenuItem>
+      </>
+    );
+  };
+
+  const AdminMenu = props => {
+    return (
+      <>
+        <SubMenu
+          className="pos-r"
+          icon={<img src={IconHome} className="h-5" alt="awSnap" />}
+          title="Dashboard"
+        >
+          {DashboardData.map((val, key) => {
+            return (
+              <MenuItem
+                id={
+                  window.location.pathname.split(val.link).pop() ? '' : 'active'
+                }
+                className="pos-r"
+                icon={<img src={val.icon} className="h-5" alt="awSnap" />}
+                key={key}
+                onClick={() => {
+                  window.location.pathname = val.link;
+                }}
+              >
+                {''}
+                {val.title}
+              </MenuItem>
+            );
+          })}
+        </SubMenu>
+        <SubMenu
+          className=""
+          icon={<img src={IconForm} className="h-5" alt="awSnap" />}
+          title="Peminjaman"
+        >
+          {PeminjamanData.map((val, key) => {
+            return (
+              <MenuItem
+                id={
+                  window.location.pathname.split(val.link).pop() ? '' : 'active'
+                }
+                className="pos-r"
+                icon={<img src={val.icon} className="h-5" alt="awSnap" />}
+                key={key}
+                onClick={() => {
+                  window.location.pathname = val.link;
+                }}
+              >
+                {''}
+                {val.title}
+                <span className="h-6 w-6 bd-rs-6 bg-danger-5 d-flex ai-center jc-center ml-a">
+                  <span className="text tc-danger-contrast">5</span>
+                </span>
+              </MenuItem>
+            );
+          })}
+        </SubMenu>
+        <SubMenu
+          className=""
+          icon={<img src={IconAdvanced} className="h-5" alt="awSnap" />}
+          title="Master"
+        >
+          {MasterData.map((val, key) => {
+            return (
+              <MenuItem
+                id={
+                  window.location.pathname.split(val.link).pop() ? '' : 'active'
+                }
+                className="pos-r"
+                icon={<img src={val.icon} className="h-5" alt="awSnap" />}
+                key={key}
+                onClick={() => {
+                  window.location.pathname = val.link;
+                }}
+              >
+                {''}
+                {val.title}
+              </MenuItem>
+            );
+          })}
+        </SubMenu>
+      </>
+    );
+  };
 
   return (
     <div className={`sidebar bg-medium-tint w-20% ${isActive ? '' : 'icon'}`}>
@@ -27,104 +191,7 @@ export function Sidebar() {
       </div>
       <ProSidebar>
         <Menu iconShape="square">
-          <SubMenu
-            className="pos-r"
-            icon={<img src={IconHome} className="h-5" alt="awSnap" />}
-            title="Dashboard"
-          >
-            {DashboardData.map((val, key) => {
-              return (
-                <MenuItem
-                  id={
-                    window.location.pathname.split(val.link).pop()
-                      ? ''
-                      : 'active'
-                  }
-                  className="pos-r"
-                  icon={<img src={val.icon} className="h-5" alt="awSnap" />}
-                  key={key}
-                  onClick={() => {
-                    window.location.pathname = val.link;
-                  }}
-                >
-                  {''}
-                  {val.title}
-                </MenuItem>
-              );
-            })}
-          </SubMenu>
-          {/* <MenuItem
-            id={window.location.pathname === '/Dashboard' ? 'active' : ''}
-            className=" pos-r"
-            icon={<img src={IconHome} className="h-5" alt="awSnap" />}
-            onClick={() => {
-              window.location.pathname = '/Dashboard';
-            }}
-          >
-            {''} Dashboard
-          </MenuItem> */}
-          <MenuItem
-            id={window.location.pathname === '/Library' ? 'active' : ''}
-            className=" pos-r"
-            icon={<img src={IconForm} className="h-5" alt="awSnap" />}
-            onClick={() => {
-              window.location.pathname = '/Library';
-            }}
-          >
-            {''} Library
-          </MenuItem>
-          <SubMenu
-            className="pos-r"
-            icon={<img src={IconUI} className="h-5" alt="awSnap" />}
-            title="Permintaan"
-          >
-            {PermintaanData.map((val, key) => {
-              return (
-                <MenuItem
-                  id={
-                    window.location.pathname.split(val.link).pop()
-                      ? ''
-                      : 'active'
-                  }
-                  className="pos-r"
-                  icon={<img src={val.icon} className="h-5" alt="awSnap" />}
-                  key={key}
-                  onClick={() => {
-                    window.location.pathname = val.link;
-                  }}
-                >
-                  {''}
-                  {val.title}
-                </MenuItem>
-              );
-            })}
-          </SubMenu>
-          <SubMenu
-            className=""
-            icon={<img src={IconAdvanced} className="h-5" alt="awSnap" />}
-            title="Master"
-          >
-            {MasterData.map((val, key) => {
-              return (
-                <MenuItem
-                  id={
-                    window.location.pathname.split(val.link).pop()
-                      ? ''
-                      : 'active'
-                  }
-                  className="pos-r"
-                  icon={<img src={val.icon} className="h-5" alt="awSnap" />}
-                  key={key}
-                  onClick={() => {
-                    window.location.pathname = val.link;
-                  }}
-                >
-                  {''}
-                  {val.title}
-                </MenuItem>
-              );
-            })}
-          </SubMenu>
+          {role === 'admin' ? <AdminMenu /> : <CustomerMenu />}
         </Menu>
         {/* <div className="w-8 h-8 bd-rs-6 d-flex jc-center ai-center ml-a mr-a pos-r p-2 bg-warning-3 btn-sidebar cur-p" onClick={handleToggle}><i className="fas fa-chevron-left"></i></div> */}
       </ProSidebar>
