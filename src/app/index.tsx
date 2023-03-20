ca806828fa71edd2422a6f48cbd22e57d5167d8f
Hanif-Ibadurrahman <irahman.hanif@gmail.com>
2023-03-20 23:14:20 +0700

    Using lazy

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,23 @@
-import { Helmet } from "react-helmet-async";
-import { Switch, BrowserRouter, Route } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { Dashboard } from "./pages/Dashboard";
-import { MasterBarang } from "./pages/Master/MasterBarang";
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export function App() {
-	const { i18n } = useTranslation();
 	localStorage.getItem("User");
 
+	const DashboardPage = lazy(() => import("app/pages/Dashboard"));
+	const HbbInventaryPage = lazy(() => import("app/pages/HbbInventory"));
+	const MasterBarangPage = lazy(() => import("app/pages/Master/MasterBarang"));
+
 	return (
-		<BrowserRouter>
-			<Switch>
-				{/* <Route path={process.env.PUBLIC_URL + "/"} component={Dashboard} /> */}
-				<Route path={process.env.PUBLIC_URL + "/"} component={MasterBarang} />
-				<Route
-					path={process.env.PUBLIC_URL + "/master/barang"}
-					component={MasterBarang}
-				/>
-			</Switch>
-		</BrowserRouter>
+		<Router>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route index element={<DashboardPage />}></Route>
+					<Route path="/dashboard" element={<DashboardPage />} />
+					<Route path="/inventaris" element={<HbbInventaryPage />} />
+					<Route path="/master" element={<MasterBarangPage />} />
+				</Routes>
+			</Suspense>
+		</Router>
 	);
 }
