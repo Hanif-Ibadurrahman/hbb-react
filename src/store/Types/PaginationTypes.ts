bb7971bab0e171e2172dbb7d144fae0f7fc63078
Hanif-Ibadurrahman <irahman.hanif@gmail.com>
2023-03-22 00:25:55 +0700

    Change to pagination response laravel

--- src/store/Types/PaginationTypes.ts
+++ src/store/Types/PaginationTypes.ts
@@ -1,6 +1,14 @@
 export interface PaginationState {
-	total: any;
-	per_page: any;
-	current_page: any;
-	last_page: any;
+	total: number;
+	per_page: number;
+	current_page: number;
+	last_page: number | null;
+	first_page_url: string | null;
+	last_page_url: string | null;
+	next_page_url: string | null;
+	prev_page_url: string | null;
+	path: string;
+	from: number;
+	to: number;
+	data: any;
 }
