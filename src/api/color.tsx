e31ec58eb3fcd9e42e146bd7f96e1b4c943441e4
Hanif-Ibadurrahman <irahman.hanif@gmail.com>
2023-04-11 23:54:09 +0700

    Stitching update, create and delete master color

--- src/api/color.tsx
+++ src/api/color.tsx
@@ -14,10 +14,15 @@ export const getAllColorApi = (params?: IColorGetAllParams) => {
 export const createNewColorApi = (input: ICreateColorRequest) => {
 	return apiWithToken.post(`/api/master/color`, input);
 };
+
 export const getDetailColorApi = (id: string) => {
 	return apiWithToken.get(`/api/master/color/${id}`);
 };
 
 export const updateColorApi = (id: string, input: IUpdateColorRequest) => {
 	return apiWithToken.put(`/api/master/color/${id}`, input);
+};
+
+export const deleteColorApi = (id: string) => {
+	return apiWithToken.delete(`/api/master/color/${id}`);
 };
--- src/app/pages/master/masterColor/components/table/columnAndDataType/index.tsx
+++ src/app/pages/master/masterColor/components/table/columnAndDataType/index.tsx
@@ -9,9 +9,10 @@ interface IColumn {
 			id?: string | undefined;
 		}>
 	>;
+	handleDelete: (id: string) => void;
 }
 
-export const columns = ({ setShowModal }: IColumn) => {
+export const columns = ({ setShowModal, handleDelete }: IColumn) => {
 	const columnType: ColumnsType<IColor> = [
 		{
 			title: "Warna",
@@ -22,7 +23,13 @@ export const columns = ({ setShowModal }: IColumn) => {
 			title: "Action",
 			dataIndex: "id",
 			render: (text, record, index) => {
-				return <ActionButtonTable setShowModal={setShowModal} itemId={text} />;
+				return (
+					<ActionButtonTable
+						setShowModal={setShowModal}
+						handleDelete={handleDelete}
+						itemId={text}
+					/>
+				);
 			},
 		},
 	];
