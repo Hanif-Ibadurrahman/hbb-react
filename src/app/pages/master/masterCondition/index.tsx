dbe8d95c7bd63a0b262cdd72a7aecfc220071395
Hanif-Ibadurrahman <irahman.hanif@gmail.com>
2023-04-11 23:27:16 +0700

    Stitching update, create and delete master condition

--- src/app/pages/master/masterCondition/index.tsx
+++ src/app/pages/master/masterCondition/index.tsx
@@ -1,14 +1,24 @@
 import { TablePaginateAndSort } from "app/components/table/antd/tablePaginateAndSort";
 import { MainLayout } from "app/layout/mainLayout";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { columns } from "./components/table/columnAndDataType";
-import { CenterModal } from "app/components/modal/centerModal";
 import { SideModal } from "app/components/modal/sideModal";
 import { SelectWithTag } from "app/components/selectWithTag";
-import { IConditionGetAllParams } from "store/types/conditionTypes";
-import { getAllConditionApi } from "api/condition";
+import { ICondition, IConditionGetAllParams } from "store/types/conditionTypes";
+import {
+	createNewConditionApi,
+	deleteConditionApi,
+	getAllConditionApi,
+	getDetailConditionApi,
+	updateConditionApi,
+} from "api/condition";
+import { Modal as AntdModal, Button, Form, FormInstance, Input } from "antd";
+import { useFormik } from "formik";
+import Swal from "sweetalert2";
 
 const MasterCondition = () => {
+	const [form] = Form.useForm();
+	const formRef = useRef<FormInstance>(null);
 	const [params, setParams] = useState<IConditionGetAllParams | undefined>();
 	const [tempFilter, setTempFilter] = useState<
 		IConditionGetAllParams | undefined
@@ -20,23 +30,133 @@ const MasterCondition = () => {
 		page: number;
 		pageSize: number;
 	}>({ page: 1, pageSize: 20 });
-	const [initialValue, setInitialValue] = useState<string | null>();
+	const [initialValue, setInitialValue] = useState<{ name: string }>();
 	const [dataTable, setDataTable] = useState();
 
 	const fetchDataList = async () => {
 		try {
 			const response = await getAllConditionApi(params);
 			setDataTable(response.data.data);
-			// await dispatch(getCountryListAction(params));
 		} catch (error: any) {
 			// CheckAuthentication(error);
 		}
+	};
+
+	const fetchDataDetail = async (id: string) => {
+		try {
+			const response = await getDetailConditionApi(id);
+			handleInitialValue(response.data.data);
+		} catch (error: any) {
+			// CheckAuthentication(error);
+		}
+	};
+
+	const handleInitialValue = (values: ICondition) => {
+		setInitialValue({ name: values.name || "" });
+		formRef.current?.setFieldsValue({ name: values.name || "" });
 	};
 
 	useEffect(() => {
 		fetchDataList();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [params]);
+
+	useEffect(() => {
+		setParams({
+			...params,
+			page: selectedPage.page,
+			page_size: selectedPage.pageSize,
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [selectedPage]);
+
+	useEffect(() => {
+		if (showModal.show && showModal.id) {
+			fetchDataDetail(showModal.id);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [showModal]);
+
+	const formik = useFormik({
+		initialValues: { name: initialValue?.name },
+		enableReinitialize: true,
+		onSubmit: values => {},
+	});
+
+	const handleAdd = () => {
+		setShowModal({ show: true });
+		setInitialValue({ name: "" });
+		formRef.current?.resetFields();
+	};
+
+	const handleDelete = (id: string) => {
+		const swalCustom = Swal.mixin({
+			customClass: {
+				confirmButton: "btn btn-success m-1",
+				cancelButton: "btn btn-danger m-1",
+			},
+			buttonsStyling: false,
+		});
+
+		swalCustom
+			.fire({
+				title: "Apakah anda yakin?",
+				text: "Ingin menghapus data ini",
+				icon: "warning",
+				showCancelButton: true,
+				confirmButtonText: "Delete",
+				cancelButtonText: "Cancel",
+				reverseButtons: true,
+			})
+			.then(result => {
+				if (result.isConfirmed) {
+					deleteConditionApi(id).then(res => {
+						if (res.data.status === "success") {
+							swalCustom.fire("Delete", "Data ini telah dihapus.", "success");
+							fetchDataList();
+						} else {
+							swalCustom.fire("Error", "Telah terjadi kesalahan", "error");
+						}
+					});
+				} else if (result.dismiss === Swal.DismissReason.cancel) {
+					swalCustom.fire("Batal", "Data ini batal dihapus", "error");
+				}
+			});
+	};
+
+	const onFinish = (values: any) => {
+		if (showModal.id) {
+			updateConditionApi(showModal.id, values).then(res => {
+				if (res.data.status === "success") {
+					setShowModal({ show: false });
+					fetchDataList();
+				}
+				Swal.fire({
+					icon: res.data.status,
+					title: res.data.message,
+					showConfirmButton: false,
+					timer: 3000,
+				});
+			});
+		} else {
+			createNewConditionApi(values).then(res => {
+				if (res.data.status === "success") {
+					setShowModal({ show: false });
+					fetchDataList();
+				}
+				Swal.fire({
+					icon: res.data.status,
+					title: res.data.message,
+					showConfirmButton: false,
+					timer: 3000,
+				});
+			});
+		}
+	};
+
+	const handleCancel = () => {
+		setShowModal({ show: false });
+	};
 
 	const setValueFilter = () => {
 		setParams({ ...params, ...tempFilter });
@@ -49,43 +169,67 @@ const MasterCondition = () => {
 						<TablePaginateAndSort
 							title="Kondisi"
 							dataSource={dataTable}
-							columns={columns({ setShowModal })}
+							columns={columns({ setShowModal, handleDelete })}
 							setSelectedPage={setSelectedPage}
+							contentHeader={
+								<button
+									type="button"
+									className="btn btn-primary"
+									onClick={handleAdd}
+								>
+									Tambah
+								</button>
+							}
 						/>
 					</div>
 				</div>
 			</section>
 
-			<CenterModal
-				modalName="modal"
-				title="Tambah Data"
-				contentFooter={
-					<button
-						type="button"
-						className="btn btn-primary"
-						data-bs-dismiss="modal"
-					>
-						Simpan
-					</button>
+			<AntdModal
+				title={showModal.show && showModal.id ? "Edit Data" : "Tambah Data"}
+				footer={
+					<div style={{ display: "flex", justifyContent: "end", columnGap: 5 }}>
+						<Button type="primary" danger onClick={handleCancel}>
+							Close
+						</Button>
+						<Button type="primary" onClick={form.submit}>
+							Simpan
+						</Button>
+					</div>
 				}
+				onCancel={handleCancel}
+				open={showModal.show}
 			>
 				<div className="col-12">
-					<div className="form-group">
-						<h6>
-							Kondisi <span className="text-danger">*</span>
-						</h6>
-						<div className="controls">
-							<input
-								type="text"
-								name="text"
-								className="form-control"
-								required
-								data-validation-required-message="This field is required"
-							/>
-						</div>
-					</div>
+					<Form form={form} ref={formRef} onFinish={onFinish}>
+						<Form.Item
+							name="name"
+							rules={[
+								{
+									required: true,
+									message: "Harap isi field ini",
+								},
+							]}
+						>
+							<div className="form-group">
+								<span>
+									Kondisi <span className="text-danger">*</span>
+								</span>
+								<div className="controls">
+									<Input
+										type="text"
+										name="name"
+										className="form-control"
+										placeholder="Kondisi"
+										onChange={formik.handleChange}
+										value={formik.values.name}
+									/>
+								</div>
+							</div>
+						</Form.Item>
+					</Form>
 				</div>
-			</CenterModal>
+			</AntdModal>
 
 			<SideModal
 				title="Filter"
