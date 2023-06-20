742aaa0c858e818905159936a97450a4a01a2acd
Hanif-Ibadurrahman <irahman.hanif@gmail.com>
2023-06-21 00:42:19 +0700

    Add approve process on task modal

--- src/app/pages/dashboard/components/modalTask/index.tsx
+++ src/app/pages/dashboard/components/modalTask/index.tsx
@@ -1,206 +1,194 @@
 import { Dispatch, SetStateAction } from "react";
-import { rejectServiceChangeApi } from "api/serviceChange";
-import { rejectServiceDeleteApi } from "api/serviceDelete";
-import { rejectServiceInspectionApi } from "api/serviceInspection";
-import { rejectServiceRepairApi } from "api/serviceRepair";
-import { rejectServiceReplacementApi } from "api/serviceReplacement";
-import { rejectServiceRequestApi } from "api/serviceRequest";
-import { rejectServiceReturnApi } from "api/serviceReturn";
+import {
+	approveServiceChangeApi,
+	rejectServiceChangeApi,
+} from "api/serviceChange";
+import {
+	approveServiceDeleteApi,
+	rejectServiceDeleteApi,
+} from "api/serviceDelete";
+import {
+	approveServiceInspectionApi,
+	rejectServiceInspectionApi,
+} from "api/serviceInspection";
+import {
+	approveServiceRepairApi,
+	rejectServiceRepairApi,
+} from "api/serviceRepair";
+import {
+	approveServiceReplacementApi,
+	getAllServiceReplacementApi,
+	rejectServiceReplacementApi,
+} from "api/serviceReplacement";
+import {
+	approveServiceRequestApi,
+	rejectServiceRequestApi,
+} from "api/serviceRequest";
+import {
+	approveServiceReturnApi,
+	rejectServiceReturnApi,
+} from "api/serviceReturn";
 import { CheckResponse } from "app/helper/authentication";
 import { Modal as AntdModal, Table, Typography } from "antd";
-import { useNavigate } from "react-router-dom";
 import { columns } from "../table/columnAndDataType";
 import Swal from "sweetalert2";
-// import { HandleReject } from "./handleReject";
+import { isAdminArea } from "app/helper/permission";
 interface IModalTask {
 	dataTable: any;
 	isShowModal: boolean;
+	updateDataList: () => void;
+	setDataForm: Dispatch<SetStateAction<any>>;
 	setIsShowModal: Dispatch<SetStateAction<boolean>>;
 	setShowModalForm: Dispatch<SetStateAction<boolean>>;
 }
 
-const ModalTask = ({ dataTable, isShowModal, setIsShowModal }: IModalTask) => {
+const ModalTask = ({
+	dataTable,
+	isShowModal,
+	updateDataList,
+	setDataForm,
+	setIsShowModal,
+	setShowModalForm,
+}: IModalTask) => {
 	const { Title } = Typography;
-	const navigate = useNavigate();
 
-	const handleApprove = record => {};
+	const fetchAndSetData = async record => {
+		try {
+			const response = await getAllServiceReplacementApi();
+			const transactionList = response.data.data.data;
 
-	const handleReject = record => {
-		const swalCustom = Swal.mixin({
-			customClass: {
-				confirmButton: "btn btn-success m-1",
-				cancelButton: "btn btn-danger m-1",
-			},
-			buttonsStyling: false,
-		});
+			const dataForm = transactionList.find(data => data.id === record.id);
+			setDataForm(dataForm);
+			setShowModalForm(true);
+		} catch (error: any) {
+			CheckResponse(error);
+		}
+	};
 
-		const swalReject = () => {
-			return swalCustom.fire(
-				"Reject",
-				"Permintaan ini telah direject.",
-				"success",
-			);
+	const swalCustom = Swal.mixin({
+		customClass: {
+			confirmButton: "btn btn-success m-1",
+			cancelButton: "btn btn-danger m-1",
+		},
+		buttonsStyling: false,
+	});
+
+	const swalApprove = () => {
+		return swalCustom.fire("Approve", "Data ini telah disetujui.", "success");
+	};
+
+	const swalReject = () => {
+		return swalCustom.fire(
+			"Reject",
+			"Permintaan ini telah direject.",
+			"success",
+		);
+	};
+
+	const swalError = () => {
+		return swalCustom.fire("Error", "Telah terjadi kesalahan", "error");
+	};
+
+	const getApproveApiFunction = transactionType => {
+		const apiFunctions = {
+			perubahan: approveServiceChangeApi,
+			penghapusan: approveServiceDeleteApi,
+			pemeriksaan: approveServiceInspectionApi,
+			perbaikan: approveServiceRepairApi,
+			penggantian: approveServiceReplacementApi,
+			permintaan: approveServiceRequestApi,
+			pengembalian: approveServiceReturnApi,
 		};
 
-		swalCustom
-			.fire({
-				title: "Apakah anda yakin ingin menolak permintaan ini?",
-				text: "Alasan penolakan",
+		return apiFunctions[transactionType];
+	};
+
+	const handleApprove = async record => {
+		if (isAdminArea && record.type === "penggantian") {
+			fetchAndSetData(record);
+		} else {
+			const result = await swalCustom.fire({
+				title: "Apakah anda yakin ingin menyetujui permintaan ini?",
+				text: "Ada catatan?",
 				input: "text",
-				inputAttributes: {
-					required: "true",
-				},
-				validationMessage: "Harap isi alasan penolakan",
 				icon: "warning",
 				showCancelButton: true,
-				confirmButtonText: "Reject",
+				confirmButtonText: "Approve",
 				cancelButtonText: "Cancel",
 				reverseButtons: true,
-			})
-			.then(result => {
-				if (result.isConfirmed) {
-					// HandleReject(record, result.value);
-					switch (record.transaction_type) {
-						case "perubahan":
-							return rejectServiceChangeApi(record.id, { remark: result.value })
-								.then(res => {
-									if (res.data.status === "success") {
-										swalReject();
-										navigate("/riwayat-tiket-layanan", {
-											replace: true,
-										});
-									} else {
-										swalCustom.fire(
-											"Error",
-											"Telah terjadi kesalahan",
-											"error",
-										);
-									}
-								})
-								.catch((error: any) => {
-									CheckResponse(error);
-								});
-						case "penghapusan":
-							return rejectServiceDeleteApi(record.id, { remark: result.value })
-								.then(res => {
-									if (res.data.status === "success") {
-										swalReject();
-										navigate("/riwayat-tiket-layanan", { replace: true });
-									} else {
-										swalCustom.fire(
-											"Error",
-											"Telah terjadi kesalahan",
-											"error",
-										);
-									}
-								})
-								.catch((error: any) => {
-									CheckResponse(error);
-								});
-
-						case "pemeriksaan":
-							return rejectServiceInspectionApi(record.id, {
-								remark: result.value,
-							})
-								.then(res => {
-									if (res.data.status === "success") {
-										swalReject();
-										navigate("/riwayat-tiket-layanan", { replace: true });
-									} else {
-										swalCustom.fire(
-											"Error",
-											"Telah terjadi kesalahan",
-											"error",
-										);
-									}
-								})
-								.catch((error: any) => {
-									CheckResponse(error);
-								});
-
-						case "perbaikan":
-							return rejectServiceRepairApi(record.id, { remark: result.value })
-								.then(res => {
-									if (res.data.status === "success") {
-										swalReject();
-										navigate("/riwayat-tiket-layanan", { replace: true });
-									} else {
-										swalCustom.fire(
-											"Error",
-											"Telah terjadi kesalahan",
-											"error",
-										);
-									}
-								})
-								.catch((error: any) => {
-									CheckResponse(error);
-								});
-
-						case "penggantian":
-							return rejectServiceReplacementApi(record.id, {
-								remark: result.value,
-							})
-								.then(res => {
-									if (res.data.status === "success") {
-										swalReject();
-										navigate("/riwayat-tiket-layanan", { replace: true });
-									} else {
-										swalCustom.fire(
-											"Error",
-											"Telah terjadi kesalahan",
-											"error",
-										);
-									}
-								})
-								.catch((error: any) => {
-									CheckResponse(error);
-								});
-
-						case "permintaan":
-							return rejectServiceRequestApi(record.id, {
-								remark: result.value,
-							})
-								.then(res => {
-									if (res.data.status === "success") {
-										swalReject();
-										navigate("/riwayat-tiket-layanan", { replace: true });
-									} else {
-										swalCustom.fire(
-											"Error",
-											"Telah terjadi kesalahan",
-											"error",
-										);
-									}
-								})
-								.catch((error: any) => {
-									CheckResponse(error);
-								});
-
-						case "pengembalian":
-							return rejectServiceReturnApi(record.id, { remark: result.value })
-								.then(res => {
-									if (res.data.status === "success") {
-										swalReject();
-										navigate("/riwayat-tiket-layanan", { replace: true });
-									} else {
-										swalCustom.fire(
-											"Error",
-											"Telah terjadi kesalahan",
-											"error",
-										);
-									}
-								})
-								.catch((error: any) => {
-									CheckResponse(error);
-								});
-
-						default:
-							break;
-					}
-				} else if (result.dismiss === Swal.DismissReason.cancel) {
-					swalCustom.fire("Batal", "Permintaan ini batal direject", "error");
-				}
 			});
+
+			if (result.isConfirmed) {
+				try {
+					const apiFunc = getApproveApiFunction(record.transaction_type);
+					const res = await apiFunc(record.id, {
+						remark: result.value,
+					});
+
+					if (res.data.status === "success") {
+						swalApprove();
+						updateDataList();
+					} else {
+						swalError();
+					}
+				} catch (error) {
+					CheckResponse(error);
+				}
+			} else if (result.dismiss === Swal.DismissReason.cancel) {
+				swalCustom.fire("Batal", "Permintaan ini batal disetujui", "error");
+			}
+		}
+	};
+
+	const getRejectApiFunction = transactionType => {
+		const apiFunctions = {
+			perubahan: rejectServiceChangeApi,
+			penghapusan: rejectServiceDeleteApi,
+			pemeriksaan: rejectServiceInspectionApi,
+			perbaikan: rejectServiceRepairApi,
+			penggantian: rejectServiceReplacementApi,
+			permintaan: rejectServiceRequestApi,
+			pengembalian: rejectServiceReturnApi,
+		};
+
+		return apiFunctions[transactionType];
+	};
+
+	const handleReject = async record => {
+		const result = await swalCustom.fire({
+			title: "Apakah anda yakin ingin menolak permintaan ini?",
+			text: "Alasan penolakan",
+			input: "text",
+			inputAttributes: {
+				required: "true",
+			},
+			validationMessage: "Harap isi alasan penolakan",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonText: "Reject",
+			cancelButtonText: "Cancel",
+			reverseButtons: true,
+		});
+
+		if (result.isConfirmed) {
+			try {
+				const apiFunc = getRejectApiFunction(record.transaction_type);
+				const res = await apiFunc(record.id, {
+					remark: result.value,
+				});
+
+				if (res.data.status === "success") {
+					swalReject();
+					updateDataList();
+				} else {
+					swalError();
+				}
+			} catch (error) {
+				CheckResponse(error);
+			}
+		} else if (result.dismiss === Swal.DismissReason.cancel) {
+			swalCustom.fire("Batal", "Permintaan ini batal direject", "error");
+		}
 	};
 
 	return (
